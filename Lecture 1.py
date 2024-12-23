ab4c0c8f7b2ad6bbbd87c70c6556eb05581f4dd7
Farhad Panahov <panahovf93@gmail.com>
2024-12-23 00:55:28 -0500

    L1 - finger exercise 1
    Finger exercise # 1

---
+++ Lecture 1.py
@@ -0,0 +1,25 @@
+# In[1]:
+# MIT course: Introduction to CS and Programming using Python
+# 6.100L | Fall 2022 | Undergraduate
+# Lecture 1
+
+
+
+
+
+# In[2]:
+# Finger exercise 1
+
+# Assume 3 variables are already defined for you: a, b, and c. 
+# Create a variable called total that adds a and b then multiplies the result by c. 
+# Include a last line in your code to print the value: print(total)
+
+
+# Solution
+a = 3
+b = 4
+c = 5
+
+total = (a+b)*c
+
+print(total)
